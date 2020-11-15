827522b8c3f76c65a3bc6aa049d693a2ca3a9b84
minsoo-web <zlemzlem5656@naver.com>
2020-11-16 00:40:06 +0900

    TRE v2

---
+++ appv2.py
@@ -0,0 +1,76 @@
+import jenkins
+import json
+import pandas as pd
+
+from configs import JenkinsConfig
+from pandas import DataFrame as df
+
+
+CONFIG = JenkinsConfig()
+
+
+def new_jenkins():
+    return jenkins.Jenkins(
+        CONFIG.url,
+        CONFIG.user,
+        CONFIG.password
+    )
+
+
+if __name__ == "__main__":
+
+    jenkins = new_jenkins()
+    build_info = jenkins.get_job_info('IRIS-E2E')
+    last_build_number = build_info['lastBuild']['number']
+
+    result = jenkins.get_build_test_report('IRIS-E2E', last_build_number)
+    # parsing
+
+    data = {
+        "Test Suites": [],
+        "Test Cases": [],
+        "Result": [],
+        "Number of Tests": []
+    }
+    count_of_testsuites = len(result["suites"])
+    cases = 0
+    success = 0
+    fail = 0
+    for suite in result["suites"]:
+        # 해당 테스트 스윗의 테스트 갯수
+        count_of_testcase = len(suite["cases"])
+        # 테스트 케이스 갯수
+        cases += count_of_testcase
+
+        # suits name
+        suite_name = suite["name"]
+        data["Test Suites"].append(suite_name)
+        data["Number of Tests"].append(count_of_testcase)
+
+        # test case
+        for case in suite["cases"]:
+            case_name = case["name"].lstrip(suite_name).lstrip()
+            data["Test Cases"].append(case_name)
+
+            if case["status"] == "PASSED":
+                data["Result"].append('✓')
+                success += 1
+            else:
+                data["Result"].append('X')
+                fail += 1
+
+        # 공백 채우기
+        for _ in range(count_of_testcase - 1):
+            data["Test Suites"].append(" ")
+            data["Number of Tests"].append(" ")
+
+    data["Test Suites"].append(f"All Test Suites: {count_of_testsuites}")
+    data["Test Cases"].append(f"All Tests: {cases}")
+    data["Result"].append(f"success: {success}")
+    data["Number of Tests"].append(f"fail: {fail}")
+
+    # print(num_of_test, num_of_suites, num_of_case, data)
+    df2 = df(data=data)
+    # print_df(df2)
+    with pd.ExcelWriter("TRE.xlsx", mode="w", engine="openpyxl") as writer:
+        df2.to_excel(writer, index=False, encoding="utf-8")
---
+++ configs.py
@@ -0,0 +1,111 @@
+"""Contains various settings to be used in this package"""
+
+
+import base64
+
+from typing import NoReturn
+
+
+class __Config:
+    """This is the base class for all settings"""
+
+    pass
+
+
+class GlobalConfig(__Config):
+    """It has settings to be used system-wide"""
+
+    pass
+
+
+class JenkinsConfig(__Config):
+    """Contains the data needed to call Jenkins API"""
+
+    def __init__(self):
+        self.__url = 'aHR0cDovL2NpY2QubW9iaWdlbi5jb20='
+        self.__user = 'ZG9uZ2h5dW4=='
+        self.__password = 'Z2hrZGVoZGd1czEh'
+
+    def __encoder(self, text: str) -> str:
+        """Encode user data by base64
+
+        Args:
+            text (str): Text to be encoded
+
+        Returns:
+            str: Encoded text
+        """
+
+        return base64.b64encode(text.encode('utf-8'))
+
+    def __decoder(self, text: str) -> str:
+        """Decode user data from base64
+
+        Args:
+            text (str): Text to be decoded
+
+        Returns:
+            str: Decoded text
+        """
+
+        return base64.b64decode(text).decode('utf-8')
+
+    @property
+    def url(self) -> str:
+        """Return your jenkins url
+
+        Returns:
+            str: Decoded jenkins url
+        """
+
+        return self.__decoder(self.__url)
+
+    @url.setter
+    def url(self, new_url: str) -> NoReturn:
+        """Set new jenkins url
+
+        Args:
+            new_url (str): You wanted new jenkins url
+        """
+
+        self.__url = self.__encoder(new_url)
+
+    @property
+    def user(self) -> str:
+        """Return your jenkins username
+
+        Returns:
+            str: Decoded jenkins username
+        """
+
+        return self.__decoder(self.__user)
+
+    @user.setter
+    def user(self, new_user: str) -> NoReturn:
+        """Set new jenkins user
+
+        Args:
+            new_user (str): You wanted new jenkins user
+        """
+
+        self.__user = self.__encoder(new_user)
+
+    @property
+    def password(self) -> str:
+        """Return your jenkins password
+
+        Returns:
+            str: Decoded jenkins password
+        """
+
+        return self.__decoder(self.__password)
+
+    @password.setter
+    def password(self, new_password: str) -> NoReturn:
+        """Set new jenkins password
+
+        Args:
+            new_password (str): You wanted new jenkins password
+        """
+
+        self.__password = self.__encoder(new_password)
